174df36e1acf2aa947d29122cdc4850e37831c1c
abraham_mh <xabr12@gmail.com>
2022-09-28 14:32:00 -0500

    jsx

---
+++ reactapp/src/index.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+var name = "Abraham"
+var apellido = "Melendez"
+var sesion = true
+
+const alumnos = ["Abraham", "Stephano", "Tom", "Jerry"];
+
+
+
+
+const jsx = (
+  <>
+    {sesion === true ? (
+      <>
+        <h1 className='saludo' style={{ color: 'green' }}>Hola {name}</h1>
+        {apellido && <p>{apellido}</p>}
+
+        <>
+          <h3>Lista</h3>
+          <ul>
+            {alumnos.map((nombre, i) => {
+              return <li key={i}>{nombre}</li>
+            })}
+          </ul>
+        </>
+
+      </>
+    ) : (
+      <h2>Primero inicia sesion</h2>
+    )}
+  </>
+
+
+);
+
+root.render(
+  <React.StrictMode>
+    {jsx}
+
+  </React.StrictMode>
+);
+
